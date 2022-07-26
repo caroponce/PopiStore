c25c1a3ee9295cc1a79ae238b60871e6faba90dd
Carolina Ponce <caroponcer@gmail.com>
2022-07-26 20:40:10 -0300

    agregue arrays con los producos

---
+++ js/site.js
@@ -0,0 +1,15 @@
+const productos = [
+    {id:1, nombre: "Eva", precio: 6000},
+    {id:2, nombre: "Bianca", precio: 6500},
+    {id:3, nombre: "Nina", precio: 3000},
+    {id:4, nombre: "Lisa", precio: 3500},
+    {id:5, nombre: "Kika", precio: 3500},
+    {id:6, nombre: "Chofa", precio: 5000},
+    {id:7, nombre: "Lola", precio: 5500},
+    {id:8, nombre: "Tita", precio: 5000},
+    {id:9, nombre: "Nura", precio: 5300},
+]
+
+console.log(productos);
+console.log(productos[0]);
+console.log(productos.precio);
