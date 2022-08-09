e9f199f0749455f289a12f33325f8a3fe8a44b21
Carolina Ponce <caroponcer@gmail.com>
2022-08-09 18:31:51 -0300

    cambios en el js

--- js/site.js
+++ js/site.js
@@ -1,87 +1,79 @@
+const temporadaVerano = [
+    {id: 1, nombre: "Eva", precio: 6000, categoria: "vestido", imagen:"multimedia/imagenes/vestidoeva.webp"},
+    {id: 2, nombre: "Bianca", precio: 6500, categoria: "vestido", imagen: "multimedia/imagenes/vestidobianca.webp"},
+    {id: 3, nombre: "Nina", precio: 3000, categoria: "top", imagen: "multimedia/imagenes/topnina.webp"},
+    {id: 4, nombre: "Lisa", precio: 3400, categoria: "top", imagen: "multimedia/imagenes/toplisa.webp"},
+    {id: 5, nombre: "Kika", precio: 3600, categoria: "top", imagen: "multimedia/imagenes/topkika.webp"},
+    {id: 6, nombre: "Chofa", precio: 5000, categoria: "falda", imagen: "multimedia/imagenes/faltachofa.webp"},
+    {id: 7, nombre: "Lola", precio: 5500, categoria: "falda", imagen: "multimedia/imagenes/faldalola.webp"},
+    {id: 8, nombre: "Tita", precio: 4000, categoria: "camisa", imagen: "multimedia/imagenes/camisatita.webp"},
+    {id: 9, nombre: "Nuri", precio: 4500, categoria: "camisa", imagen: "multimedia/imagenes/camisanuri.webp"},
+]
 
-class producto {
-    constructor (id, nombre, precio,categoria) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = parseFloat(precio);
-        this.categoria = categoria;
-    }
-}
-
-const temporadaVerano = []
-
-temporadaVerano.push(new producto (0, "Eva", 6000, "vestido"));
-temporadaVerano.push(new producto (1, "Bianca", 6500, "vestido"));
-temporadaVerano.push(new producto (2, "Nina", 3000, "top"));
-temporadaVerano.push(new producto (3, "Lisa", 3500, "top"));
-temporadaVerano.push(new producto (4, "Kika", 3500, "top"));
-temporadaVerano.push(new producto (5, "Chofa", 5000, "falda"));
-temporadaVerano.push(new producto (6, "Lola", 5500, "falda"));
-temporadaVerano.push(new producto (7, "Tita", 5500, "camisa"));
-temporadaVerano.push(new producto (8, "Nuri", 5300, "camisa"));
-
+const carritoCompras = [];
 
 let nombreUsuario = prompt("Registrate dejando tu nombre")
 
 let bienvenido = document.getElementById("bienvenido");
 bienvenido.innerText = `Bienvenido/a ${nombreUsuario}. Gracias por registraste`
 
-let numero = 0
-        while (numero < 8) {
-         numero++;
-        let listaProductos = document.createElement("div");
-        listaProductos.innerHTML = `<h3> Producto:${temporadaVerano[numero].nombre}</h3>
-                                    <br />  <b>$: ${temporadaVerano[numero].precio} </b> `
-        document.body.appendChild(listaProductos);
-                        }
 
-let itemLista = document.getElementById("consultas-gral");
 
-itemLista.onmouseover = () => console.log("Metodos de pago : Transferencia, tarjeta de débito y tarjeta de crédito, Envios a todos el pais por correo argentino, 30 días para devoluciones");
-   
-let carritoEva = document.getElementById("carrito-eva");
-carritoEva.addEventListener("click", agregarCarrito1);
-function agregarCarrito1() {
-    console.log("Se ha agregado el producto " + temporadaVerano[0].nombre + " al carrito $" + temporadaVerano[0].precio)
-    }
+let formaDePago = document.getElementById("formaPago");
+let formaDeEnvio = document.getElementById("formaEnvio");
+let devoluciones = document.getElementById("devoluciones")
 
-let carritoBianca = document.getElementById("carrito-bianca");
-carritoBianca.addEventListener("click", agregarCarrito2);
-function agregarCarrito2() {
-    console.log("Se ha agregado el producto " + temporadaVerano[1].nombre + " al carrito $" + temporadaVerano[1].precio)
-        }
-let carritoNina = document.getElementById("carrito-nina");
-carritoNina.addEventListener("click", agregarCarrito3);
-function agregarCarrito3() {
-console.log("Se ha agregado el producto " + temporadaVerano[2].nombre + " al carrito $" + temporadaVerano[2].precio)
-          }
-let carritoLisa = document.getElementById("carrito-lisa");
-carritoLisa.addEventListener("click", agregarCarrito4);
-function agregarCarrito4() {
-console.log("Se ha agregado el producto " + temporadaVerano[3].nombre + " al carrito $" + temporadaVerano[3].precio)
-        }
-let carritoKika = document.getElementById("carrito-kika");
-carritoKika.addEventListener("click", agregarCarrito5);
-function agregarCarrito5() {
-console.log("Se ha agregado el producto " + temporadaVerano[4].nombre + " al carrito $" + temporadaVerano[4].precio)
-        }
-let carritoChofa = document.getElementById("carrito-chofa");
-carritoChofa.addEventListener("click", agregarCarrito6);
-function agregarCarrito6() {
-console.log("Se ha agregado el producto " + temporadaVerano[5].nombre + " al carrito $" + temporadaVerano[5].precio)
-                }
-let carritoLola = document.getElementById("carrito-lola");
-carritoLola.addEventListener("click", agregarCarrito7);
-function agregarCarrito7() {
- console.log("Se ha agregado el producto " + temporadaVerano[6].nombre + " al carrito $" + temporadaVerano[6].precio)
-     }
-let carritoTita = document.getElementById("carrito-tita");
-carritoTita.addEventListener("click", agregarCarrito8);
-function agregarCarrito8() {
-console.log("Se ha agregado el producto " + temporadaVerano[7].nombre + " al carrito $" + temporadaVerano[7].precio)
-          }
-let carritoNuri = document.getElementById("carrito-nuri");
-carritoNuri.addEventListener("click", agregarCarrito9);
-function agregarCarrito9() {
-console.log("Se ha agregado el producto " + temporadaVerano[8].nombre + " al carrito $" + temporadaVerano[8].precio)
-                    }
+formaDePago.addEventListener("mousemove", (e) => {
+    return formaDePago.innerText = "15%off en efectivo. 3/6 cuotas sin interes"
+    
+})
+
+formaDeEnvio.addEventListener("mousemove", (e) => {
+    return formaDeEnvio.innerText = "Envios a todos el pais. Envios a CABA y GBA en 24hs"
+})
+
+devoluciones.addEventListener("mousemove", (e) => {
+    return devoluciones.innerHTML = "Devoluciones dentro de los 15 días de recibida la compra"
+})
+
+const contenedorProductos = document.getElementById("contenedor-productos");
+const contenedorCarrito = document.getElementById("contenedor-carrito");
+const totalCompra = document.getElementById("total-compra");
+const agregarAlCarrito = (produId) => {
+    
+    const itemCarrito = temporadaVerano.find((produ) => produ.id === produId);
+    carritoCompras.push(itemCarrito);
+    ActualizarCarrito();
+}
+
+temporadaVerano.forEach((productos) => {
+    const div = document.createElement("div");
+    div.classList.add("productos");
+    div.innerHTML = `
+    <div class"productos${productos.id}">
+    <img class="foto-producto" src="${productos.imagen}">
+    <h2 class="nombre-producto" > ${productos.nombre} </h2>
+    <p>  $${productos.precio} </p>
+    <button id="boton${productos.id}" class = "boton"> Agregar al carrito </button>
+    </div>
+    `
+    contenedorProductos.appendChild(div)
+    const botonCompra = document.getElementById(`boton${productos.id}`);
+    botonCompra.addEventListener("click", () => {
+        agregarAlCarrito(productos.id)
+    });;
+})
+
+const ActualizarCarrito = () => {
+contenedorCarrito.innerHTML = "";
+carritoCompras.forEach((productoCarrito) => {
+    const divCarrito = document.createElement("div");
+    divCarrito.classList.add("productoCarrito")
+    divCarrito.innerHTML = `
+    <p> ${productoCarrito.nombre} </p>
+    <p> $${productoCarrito.precio} </p> `
+    contenedorCarrito.appendChild(divCarrito)
+    totalCompra.innerHTML = "Total: $" + carritoCompras.reduce ((acc, productoCarrito) => acc + productoCarrito.precio, 0)
+    localStorage.setItem(productoCarrito.nombre, productoCarrito.precio)
+})
+}
